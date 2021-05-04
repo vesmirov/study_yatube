99a5fdf068365fd4f51429376398643c967fdcdf
Evan <evan.vilagov@gmail.com>
2021-05-04 19:56:45 +0300

    Added some serializers (Post, Group, User)

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets, mixins
 
-# Create your views here.
+from .serializers import UserSerializer
+
+User = get_user_model()
+
+
+class UserModelViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
